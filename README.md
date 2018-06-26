6ad47b16bccdc940bfec4d3bd18add5551032a98
CRAIGSPC\craig <craig@optimalworks.net>
2018-06-26 16:49:04 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # gulp-css
 Using Gulp.js for CSS tasks
+
+
+## Build production CSS
+Linux/Mac OS:
+
+```bash
+NODE_ENV=production
+gulp css
+```
+
+(or inline `NODE_ENV=production gulp build`)
+
+Windows Powershell:
+
+```powershell
+$env:NODE_ENV="production"
+gulp css
+```
+
+Windows legacy command line:
+
+```cmd
+set NODE_ENV=production
+gulp css
+```
---
+++ gulpfile.js
@@ -0,0 +1,133 @@
+(() => {
+
+  'use strict';
+
+  /**************** Gulp.js configuration ****************/
+
+  const
+
+    // development or production
+    devBuild  = ((process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development'),
+
+    // directory locations
+    dir = {
+      src         : 'src/',
+      build       : 'build/'
+    },
+
+    // modules
+    gulp          = require('gulp'),
+    noop          = require('gulp-noop'),
+    newer         = require('gulp-newer'),
+    size          = require('gulp-size'),
+    imagemin      = require('gulp-imagemin'),
+    sass          = require('gulp-sass'),
+    postcss       = require('gulp-postcss'),
+    sourcemaps    = devBuild ? require('gulp-sourcemaps') : null,
+
+    browsersync   = require('browser-sync').create();
+
+
+  console.log('Gulp', devBuild ? 'development' : 'production', 'build');
+
+
+  /**************** images task ****************/
+
+  const imgConfig = {
+    src           : dir.src + 'images/**/*',
+    build         : dir.build + 'images/',
+
+    minOpts: {
+      optimizationLevel: 5
+    }
+  };
+
+  gulp.task('images', () =>
+
+    gulp.src(imgConfig.src)
+      .pipe(newer(imgConfig.build))
+      .pipe(imagemin(imgConfig.minOpts))
+      .pipe(size({ showFiles:true }))
+      .pipe(gulp.dest(imgConfig.build))
+
+  );
+
+
+  /**************** CSS task ****************/
+
+  const cssConfig = {
+
+    src         : dir.src + 'scss/main.scss',
+    watch       : dir.src + 'scss/**/*',
+    build       : dir.build + 'css/',
+    sassOpts: {
+      sourceMap       : devBuild,
+      outputStyle     : 'nested',
+      imagePath       : '/images/',
+      precision       : 3,
+      errLogToConsole : true
+    },
+
+    postCSS: [
+      require('postcss-assets')({
+        loadPaths: ['images/'],
+        basePath: dir.build
+      }),
+      require('autoprefixer')({
+        browsers: ['> 1%']
+      })
+    ]
+
+  };
+
+  // minify production CSS
+  if (!devBuild) {
+    cssConfig.postCSS.push(require('cssnano'));
+  }
+
+  gulp.task('css', ['images'], () =>
+
+    gulp.src(cssConfig.src)
+      .pipe(sourcemaps ? sourcemaps.init() : noop())
+      .pipe(sass(cssConfig.sassOpts))
+      .pipe(postcss(cssConfig.postCSS))
+      .pipe(sourcemaps ? sourcemaps.write() : noop())
+      .pipe(size({ showFiles:true }))
+      .pipe(gulp.dest(cssConfig.build))
+      .pipe(browsersync ? browsersync.reload({ stream: true }) : noop())
+
+  );
+
+
+  /**************** browser-sync task ****************/
+
+  const syncConfig = {
+    server: {
+      baseDir   : './',
+      index     : 'index.html'
+    },
+    port        : 8000,
+    files       : dir.build + '**/*',
+    open        : false
+  };
+
+  // browser-sync
+  gulp.task('browsersync', () =>
+    browsersync ? browsersync.init(syncConfig) : null
+  );
+
+
+  /**************** watch task ****************/
+
+  gulp.task('default', ['css', 'browsersync'], () => {
+
+    // image changes
+    gulp.watch(imgConfig.src, ['images']);
+
+    // CSS changes
+    gulp.watch(cssConfig.watch, ['css']);
+
+  });
+
+
+})();
